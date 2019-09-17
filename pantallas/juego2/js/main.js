1ca0a6a0d3031dd115ff4cbec2bcff17698afe04
Jorge Isaac Pozos <shako.ik@gmail.com>
2019-09-16 23:21:13 -0500

    Juego2: Avance

--- pantallas/juego2/js/main.js
+++ pantallas/juego2/js/main.js
@@ -2,9 +2,24 @@ var alebrije={
   cabeza: {
       id: '',
       x:0,
-      y:0
+      y:0,
+      image: '',
+      origen: ''
+  },
+  patas: {
+    id: '',
+    x: 0,
+    y: 0,
+    image: '',
+    origen: ''
   }
 };
+var screenPosition = {
+  x: 0,
+  y: 0
+};
+var categoriaSelecc;
+var dentroDropzone = false;
 $(document).ready(empezarJuego);
 
 function empezarJuego(){
@@ -12,38 +27,106 @@ function empezarJuego(){
     $('.footer').fadeOut('slow');
 
     interact('#dropzone').dropzone({
+      accept: '.dropIN',
+      checker: function (
+        dragEvent,         // related dragmove or dragend
+        event,             // Touch, Pointer or Mouse Event
+        dropped,           // bool default checker result
+        dropzone,          // dropzone Interactable
+        dropElement,       // dropzone element
+        draggable,         // draggable Interactable
+        draggableElement) {// draggable element
+        
+        var obj = event.target;
+        screenPosition.x = event.x;
+        screenPosition.y = event.y;
+        
+        dentroDropzone = dropped;
+        if(dentroDropzone) {
+          obj.classList.add('can-drop');
+        } else {
+          obj.classList.remove('can-drop');
+          
+        }
+
+      },
       ondropactivate: function (event) {
         // add active dropzone feedback
-        event.target.classList.add('drop-active')
+        event.target.classList.add('drop-active');
+        
+        //console.log(event.relatedTarget.offsetLeft+', '+event.relatedTarget.offsetTop);
+        if(categoriaSelecc === 'menuCabezas'){
+          if(alebrije.cabeza.id!=='' && alebrije.cabeza.id !== event.relatedTarget.id){ // No es el primer objeto y no es el mismo objeto
+            idCabeza = '#'+alebrije.cabeza.id;
+            origen = alebrije.cabeza.origen;
+            
+            $(idCabeza).remove();
+            $(origen).removeClass('empty').html(alebrije.cabeza.image);
+            $(idCabeza).removeAttr('style').data('x', 0).data('y', 0);
+            
+          }
+        }else if(categoriaSelecc === 'menuCabezas'){
+          if(alebrije.patas.id!=='' && alebrije.patas.id !== event.relatedTarget.id){ // No es el primer objeto y no es el mismo objeto
+            idPatas = '#'+alebrije.patas.id;
+            origen = alebrije.patas.origen;
+            
+            $(idPatas).remove();
+            $(origen).removeClass('empty').html(alebrije.patas.image);
+            $(idPatas).removeAttr('style').data('x', 0).data('y', 0);
+            
+          }
+        }
       },
       ondropdeactivate: function (event) {
         // remove active dropzone feedback
         event.target.classList.remove('drop-active');
+        event.relatedTarget.classList.remove('can-drop');
+        event.relatedTarget.dataset.x=0;
+        event.relatedTarget.dataset.y=0;
+        // console.log(event);
         
-      },
-      ondragenter: function(event){
-        
-        if(alebrije.cabeza.id!=''){
-          console.log(alebrije.cabeza.id);
-          idCabeza = '#'+alebrije.cabeza.id;
-          $(idCabeza).removeAttr('style').data('x',0).data('y',0);
+        if(dentroDropzone){
+          // El objeto se ha depositado en la zona
+
+          imgPos=[];
+          imgPos.push( screenPosition.x - 260 );
+          imgPos.push( screenPosition.y - 290 );
+          
+          if(categoriaSelecc==='menuCabezas'){
+            // Insertamos datos en el objeto
+            
+            alebrije.cabeza.id = event.relatedTarget.id;
+            alebrije.cabeza.image = event.relatedTarget.outerHTML;
+            alebrije.cabeza.origen = event.relatedTarget.offsetParent;
+          }else if(categoriaSelecc==='menuPatas'){
+            // Insertamos datos en el objeto
+            
+            alebrije.patas.id = event.relatedTarget.id;
+            alebrije.patas.image = event.relatedTarget.outerHTML;
+            alebrije.patas.origen = event.relatedTarget.offsetParent;
+          }
+          
+          imgDropped=event.relatedTarget;
+          
+          event.relatedTarget.offsetParent.classList.add('empty');
+          event.relatedTarget.classList.remove('dropIN');
+          event.relatedTarget.dataset.x=imgPos[0];
+          event.relatedTarget.dataset.y=imgPos[1];
+          event.relatedTarget.style.transform="translate("+imgPos[0]+"px, "+imgPos[1]+"px)";
+          $('#dropzone').append(imgDropped);
+          
+        }else{
+          event.relatedTarget.style.transform='translate(0px, 0px)';
+          // update the posiion attributes
+          event.relatedTarget.setAttribute('data-x', 0);
+          event.relatedTarget.setAttribute('data-y', 0);
         }
-      },
-      ondragleave: function (event) {
-        //console.log('no està dentro de dropzone');
-        event.relatedTarget.style.transform='translate(0px, 0px)';
-        // update the posiion attributes
-        event.target.setAttribute('data-x', 0);
-        event.target.setAttribute('data-y', 0);
-      },
-      ondrop: function(event){
-         alebrije.cabeza.id = event.relatedTarget.id;
+        
       }
     });
 
     interact('.dragg').draggable({
       inertia: true,
-      onstart: recordPosition,
       onmove: dragMoveListener
     });
 }
@@ -53,14 +136,10 @@ function abrirMneu(){
     $('.menuJ2').find('.activo').removeClass('activo');
 
     $(this).addClass('activo');
+    categoriaSelecc = $(this).data('sbmenu');
     var $submenu = '#'+$(this).data('sbmenu');
     $($submenu).addClass('activo');
  
-}
-function recordPosition(event){
-  console.log(event.dx+', '+event.dy);
-   
-
 }
 function dragMoveListener (event) {
     var target = event.target
@@ -69,9 +148,7 @@ function dragMoveListener (event) {
     var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
   
     // translate the element
-    target.style.webkitTransform =
-      target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)'
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
   
     // update the posiion attributes
     target.setAttribute('data-x', x)
