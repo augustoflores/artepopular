90a7d4da9e8fd2243fa0c7f06fc2e3d066117f96
augustoflores <augustoflores@gmail.com>
2019-09-17 09:45:21 -0500

    main menu added

--- js/globos.js
+++ js/globos.js
@@ -2,12 +2,28 @@ $(function () {
   let indexglobo = -1;
   let arrGlobos = $(".globostexto .globotexto");
   let arrImages = $(".imagecontainer .image");
+  let texto="";
   $(".globonext").click(function () {
     if (indexglobo < (arrGlobos.length - 1)) {
       indexglobo++;
       $(".globostexto .globotexto").hide(0);
       $(".imagecontainer .image").hide(0);
+      texto = $(arrGlobos[indexglobo]).html();
+      $(arrGlobos[indexglobo]).html("")
       $(arrGlobos[indexglobo]).fadeIn(0);
+
+      var app = $(arrGlobos[indexglobo])[0];
+
+      var typewriter = new Typewriter(app, {
+          loop: false,
+          delay: 30,
+          cursor: ""
+      });
+      console.log(typewriter)
+      typewriter.typeString(texto)
+          .pauseFor(0)
+          .start();
+      
       $(arrImages[indexglobo]).fadeIn(0);
       return true;
     }
--- pantallas/juego4/js/main.js
+++ pantallas/juego4/js/main.js
@@ -2,56 +2,120 @@ $(function () {
   var arrSecuencia = [];
   var arrSecuenciaUsuario = [];
   var turnoUsuario = false
-  var iniciado=false;
-  var sonando=false;
-  function inicia () {
+  var iniciado = false;
+  var sonando = false;
+  nivelactual = 0;
+  var textoactual = 0;
+  var niveles = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [8, 7, 6]
+  ]
+  var arrTextos = [
+    "Comienza con el bordado de abajo. Presiona iniciar comenzará una secuencia de 4 tiempos, al finalizar, pulsa en el mismo orden para desbloquear el siguiente nivel.",
+    "Excelente, ahora vamos con el patrón de arriba",
+    "Excelente, ahora vamos con último patrón.",
+    "¡Felicidades lo lograste! Has ganado la Medalla de Gran Maestro Artesano María Pérez te has ganado la siguiente insignia como recompensa. Telar Corazón de Chamula"
+  ]
+  $("#iniciar").hide();
+  $("#continuar").hide();
+
+  function crearsecuencia(nivel) {
+    arrNivel = [];
+    notas = niveles[nivel];
+    for (let index = 0; index < 4; index++) {
+      cual = Math.floor(Math.random() * 3);
+      console.log(cual);
+      arrNivel.push(notas[cual]);
+    }
+    return arrNivel;
+  }
+
+  function inicia() {
     arrSecuencia = [];
     arrSecuenciaUsuario = [];
     turnoUsuario = false
-    iniciado=false;
-    sonando=false;
+    iniciado = false;
+    sonando = false;
   }
   const synth = new Tone.AMSynth().toMaster()
+  $(".pieza").css("opacity", .0);
+  $("#comenzar").click(function (event) {
+    $(this).hide();
+    $("#iniciar").show();
+    animartexto(".globotexto", arrTextos[textoactual]);
+    console.log("textoactual: ", textoactual);
+  });
   $("#iniciar").click(function (event) {
     $(this).hide();
     turnoMaquina();
   })
+  $("#continuar").click(function (event) {
+    $(this).hide();
+    turnoMaquina();
+  })
   $(".pieza").click(function (event) {
-    if(sonando) return false
-    sonando=true;
-    note=$(this).data("note");
-    id=$(this).attr("id");
-    num=Number(id.replace("pieza",""));
-    if(turnoUsuario && iniciado){
-      console.log("usuario: ",arrSecuenciaUsuario)
+    //inicia
+    if (sonando) return false
+    sonando = true;
+    note = $(this).data("note");
+    id = $(this).attr("id");
+    num = Number(id.replace("pieza", ""));
+    var final = false;
+    if (turnoUsuario && iniciado) {
+      console.log("usuario: ", arrSecuenciaUsuario)
       arrSecuenciaUsuario.push(num);
-      arrSecuenciaTest=arrSecuencia.slice(0,arrSecuenciaUsuario.length)
-      if(JSON.stringify(arrSecuenciaUsuario) == JSON.stringify(arrSecuenciaTest)){
-        if(arrSecuenciaUsuario.length===arrSecuencia.length){
-          turnoMaquina();
+      arrSecuenciaTest = arrSecuencia.slice(0, arrSecuenciaUsuario.length)
+      if (JSON.stringify(arrSecuenciaUsuario) == JSON.stringify(arrSecuenciaTest)) {
+        if (arrSecuenciaUsuario.length === arrSecuencia.length) {
+          final = true;
+          for (let index = 0; index < niveles[nivelactual].length; index++) {
+            const element = niveles[nivelactual][index];
+            console.log("#pieza" + element);
+            $("#pieza" + element).css("opacity", 1)
+          }
+
+          nivelactual++;
+          textoactual++;
+          if(textoactual==3){
+            $("#imagen").attr("src","./img/telar.png")
+          }
+          
+          animartexto(".globotexto", arrTextos[textoactual]);
+          sonando = false;
+          if (nivelactual < 3) {
+            $("#continuar").show();
+            setTimeout(() => {
+              //turnoMaquina();
+            }, 2000)
+          }
           $("#retro").html("Muy bien, sigue jugando");
         }
-      }else{
+      } else {
         $("#retro").html("Te equivocaste, vuelve a intentar");
         inicia();
         $("#inicar").show();
       }
     };
     $(this).css("color", "yellow");
-    setTimeout(() => {$(this).css("color", "white");sonando=false;}, 1000);
-    //var mySound = new buzz.sound("./sounds/click.m4a");
-    //mySound.play();
-    synth.triggerAttackRelease(note+'4', '8n')
+    $(this).css("opacity", 1)
+    if (!final) {
+      setTimeout(() => {
+        $(this).css("color", "white");
+        $(this).css("opacity", .0);
+        sonando = false;
+      }, 800);
+    }
+    synth.triggerAttackRelease(note, '8n')
   })
-  
+
   function turnoMaquina() {
-    iniciado=true;
-    turnoUsuario=false;
+    iniciado = true;
+    turnoUsuario = false;
     arrSecuenciaUsuario = [];
     $(".pieza").css("pointer-events", "none")
-    var random = Math.floor(Math.random() * $(".pieza").length);
-    arrSecuencia.push(random)
-    console.log("Maquina: ",arrSecuencia);
+    arrSecuencia = crearsecuencia(nivelactual);
+    console.log("Maquina: ", arrSecuencia);
     maquinaToca()
   }
 
@@ -61,7 +125,7 @@ $(function () {
     var interval = setInterval(() => {
       if (index >= arrSecuencia.length) {
         $(".pieza").css("pointer-events", "auto")
-        turnoUsuario=true;
+        turnoUsuario = true;
         clearInterval(interval);
         $("#turno").html("Repite el patron");
         return false;
@@ -69,5 +133,19 @@ $(function () {
       $("#pieza" + arrSecuencia[index]).click()
       index++;
     }, 1500);
+  }
+  animartexto(".globotexto");
+
+  function animartexto(selector, texto) {
+    if (!texto) texto = $(selector).html();
+    var app = $(selector)[0];
+    var typewriter = new Typewriter(app, {
+      loop: false,
+      delay: 10,
+      cursor: ""
+    });
+    typewriter.typeString(texto)
+      .pauseFor(0)
+      .start();
   }
 });
